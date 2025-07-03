77eb6dbcea6fbc89111f6ce9ac3c213759150139
Estefania-2023 <estefa20or@gmail.com>
2025-07-03 16:31:54 -0500

    week1 - assigment

--- vite-project/src/TodoItem.jsx
+++ vite-project/src/TodoItem.jsx
@@ -5,7 +5,7 @@ export function TodoItem( {completed, id, title, toggleTodo, deleteTodo} ) {
         <input type="checkbox" checked={completed} onChange={e => toggleTodo (id, e.target.checked)}/>
         {title}
       </label>
-      <button onClick={() => deleteTodo(id)} className="btn btn-danger">Delete</button>
+      <button onClick={() => deleteTodo(id)} className="btn-danger">Delete</button>
     </li>
     )
 }
